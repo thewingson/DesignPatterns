3eb460b8940d2f6299f8400f4e2e84259811a485
thewingson <almatrakh97@gmail.com>
2020-12-30 22:44:51 +0600

    implementation of builder

---
+++ src/kz/almat/Builder/ChineseHouseBuilder.java
@@ -0,0 +1,31 @@
+package kz.almat.Builder;
+
+public class ChineseHouseBuilder implements HouseBuilder {
+
+    private String walls;
+    private String doors;
+    private String windows;
+
+    @Override
+    public HouseBuilder setWalls(String walls) {
+        this.walls = walls;
+        return this;
+    }
+
+    @Override
+    public HouseBuilder setDoors(String doors) {
+        this.doors = doors;
+        return this;
+    }
+
+    @Override
+    public HouseBuilder setWindows(String windows) {
+        this.windows = windows;
+        return this;
+    }
+
+    @Override
+    public House build() {
+        return new House(this.walls, this.doors, this.windows);
+    }
+}
