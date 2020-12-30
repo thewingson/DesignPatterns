bf9b106fd8558612c805a4251c588c7b4ccac40a
thewingson <almatrakh97@gmail.com>
2020-12-30 22:53:19 +0600

    running

--- src/kz/almat/Builder/Starter.java
+++ src/kz/almat/Builder/Starter.java
@@ -5,6 +5,9 @@ public class Starter {
     public static void main(String[] args) {
         Pizza pizza = new Pizza.PizzaBuilder().size(15).cheese("bolonese").onion("red").topping("salami").build();
         System.out.println(pizza);
+
+        House house = new ChineseHouseBuilder().setWalls("walls").setDoors("doors").setWindows("windows").build();
+        System.out.println(house);
     }
 
 }
