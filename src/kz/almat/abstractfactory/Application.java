faacfe8eb52543f0acf2e71a29ba173de980a2e4
thewingson <almatrakh97@gmail.com>
2020-12-31 01:23:26 +0600

    configuration point

---
+++ src/kz/almat/abstractfactory/Application.java
@@ -0,0 +1,17 @@
+package kz.almat.abstractfactory;
+
+public class Application {
+
+    private Button button;
+    private Form form;
+
+    public Application(AbstractFactory factory) {
+        this.button = factory.createButton();
+        this.form = factory.createForm();
+    }
+
+    public void draw(){
+        button.draw();
+        form.draw();
+    }
+}
