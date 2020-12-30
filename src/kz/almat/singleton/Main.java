9f2a84cbe1e740878125ded71d5c2e326f27f9d5
thewingson <almatrakh97@gmail.com>
2020-12-30 21:41:10 +0600

    checking implementation

--- src/kz/almat/singleton/Main.java
+++ src/kz/almat/singleton/Main.java
@@ -5,7 +5,31 @@ public class Main {
     public static void main(String[] args) {
         EagerSingleton eagerSingleton = EagerSingleton.getInstance();
         EagerSingleton eagerSingleton2 = EagerSingleton.getInstance();
-        System.out.print("Are singleton instances the same? ");
+        System.out.print("EagerSingleton: ");
         System.out.println(eagerSingleton == eagerSingleton2);
+
+        System.out.println();
+        System.out.println("---------------------------------");
+        Thread thread = new Thread(new ThreadLazySingletonOne());
+        Thread thread2 = new Thread(new ThreadLazySingletonTwo());
+        System.out.println("LazySingleton: ");
+        thread.start();
+        thread2.start();
+    }
+
+    static class ThreadLazySingletonOne implements Runnable {
+        @Override
+        public void run() {
+            LazySingleton singleton = LazySingleton.getInstance("FOO");
+            System.out.println(singleton.value);
+        }
+    }
+
+    static class ThreadLazySingletonTwo implements Runnable {
+        @Override
+        public void run() {
+            LazySingleton singleton = LazySingleton.getInstance("BAR");
+            System.out.println(singleton.value);
+        }
     }
 }
