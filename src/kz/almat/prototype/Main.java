bbbcadb3fa0122a57b83bebe890c5002d93ac259
thewingson <almatrakh97@gmail.com>
2020-12-31 00:05:04 +0600

    initialization

---
+++ src/kz/almat/prototype/Main.java
@@ -0,0 +1,12 @@
+package kz.almat.prototype;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Animal cat = new Cat("black", "Tom");
+        Animal clone = cat.clone();
+        System.out.println(cat == clone);
+        System.out.println(cat.equals(clone));
+    }
+
+}
