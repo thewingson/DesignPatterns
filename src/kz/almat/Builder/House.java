5d0ea9a584b1b2fce85a888d37cbf24bbbf66cc0
thewingson <almatrakh97@gmail.com>
2020-12-30 22:52:50 +0600

    added new parameter

--- src/kz/almat/Builder/House.java
+++ src/kz/almat/Builder/House.java
@@ -5,11 +5,22 @@ public class House {
     private String walls;
     private String doors;
     private String windows;
+    private HouseType type;
 
-    public House(String walls, String doors, String windows){
+    public House(String walls, String doors, String windows, HouseType type){
         this.walls = walls;
         this.doors = doors;
         this.windows = windows;
+        this.type = type;
     }
 
+    @Override
+    public String toString() {
+        return "House{" +
+                "walls='" + walls + '\'' +
+                ", doors='" + doors + '\'' +
+                ", windows='" + windows + '\'' +
+                ", type=" + type +
+                '}';
+    }
 }
