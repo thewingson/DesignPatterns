6e882ee9205efae0b9df02e4c22da4d7dc589d6b
thewingson <almatrakh97@gmail.com>
2020-12-30 22:44:24 +0600

    class for House

---
+++ src/kz/almat/Builder/House.java
@@ -0,0 +1,15 @@
+package kz.almat.Builder;
+
+public class House {
+
+    private String walls;
+    private String doors;
+    private String windows;
+
+    public House(String walls, String doors, String windows){
+        this.walls = walls;
+        this.doors = doors;
+        this.windows = windows;
+    }
+
+}
