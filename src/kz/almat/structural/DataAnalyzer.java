5d1b5bb4acfa71ce5d8d47a5a83401c77d4d526d
thewingson <almatrakh97@gmail.com>
2021-01-02 02:28:34 +0600

    simple consumer

---
+++ src/kz/almat/structural/DataAnalyzer.java
@@ -0,0 +1,9 @@
+package kz.almat.structural;
+
+public class DataAnalyzer {
+
+    public static void analyze(Message32bit message){
+        System.out.println(message.getResult32bit());
+    }
+
+}
