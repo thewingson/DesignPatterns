41be319d0b03b403403f589bfca5e4422b4b24a8
thewingson <almatrakh97@gmail.com>
2021-01-02 02:27:32 +0600

    provider type message

---
+++ src/kz/almat/structural/Message64bit.java
@@ -0,0 +1,17 @@
+package kz.almat.structural;
+
+public class Message64bit {
+    private String message;
+
+    public Message64bit(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
