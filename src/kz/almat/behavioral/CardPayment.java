37557f2ceee49e5d5603b88a24dfa4ec05d7cf5d
thewingson <almatrakh97@gmail.com>
2021-01-03 13:59:30 +0600

    strategy for card payment

---
+++ src/kz/almat/behavioral/CardPayment.java
@@ -0,0 +1,17 @@
+package kz.almat.behavioral;
+
+public class CardPayment implements PaymentStrategy {
+
+    public CardPayment() {
+    }
+
+    @Override
+    public void pay() {
+        System.out.println("Asking to insert card...");
+        System.out.println("Asking for PIN...");
+        System.out.println("Checking PIN...");
+        System.out.println("Asking for cash...");
+        System.out.println("Checking balance...");
+        System.out.println("Withdraw cash...");
+    }
+}
