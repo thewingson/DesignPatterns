403168bdb1b3aa9089aa198f5eeb3c3f12cc195f
thewingson <almatrakh97@gmail.com>
2021-01-05 02:07:13 +0600

    added entities to visitor

--- src/kz/almat/behavioral/visitor/ReportVisitor.java
+++ src/kz/almat/behavioral/visitor/ReportVisitor.java
@@ -1,11 +1,15 @@
 package kz.almat.behavioral.visitor;
 
+import kz.almat.behavioral.visitor.model.Employee;
+import kz.almat.behavioral.visitor.model.Order;
+import kz.almat.behavioral.visitor.model.Product;
+
 public interface ReportVisitor {
 
-    String visitProductReport();
+    String visitProductReport(Product product);
 
-    String visitOrderReport();
+    String visitOrderReport(Order order);
 
-    String visitEmployeeReport();
+    String visitEmployeeReport(Employee employee);
 
 }
