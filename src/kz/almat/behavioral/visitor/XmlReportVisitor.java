9eb963b21d52c813f7ea667eed6eef379d2b59c4
thewingson <almatrakh97@gmail.com>
2021-01-05 02:27:22 +0600

    concrete visitor for xml report

---
+++ src/kz/almat/behavioral/visitor/XmlReportVisitor.java
@@ -0,0 +1,22 @@
+package kz.almat.behavioral.visitor;
+
+import kz.almat.behavioral.visitor.model.Employee;
+import kz.almat.behavioral.visitor.model.Order;
+import kz.almat.behavioral.visitor.model.Product;
+
+public class XmlReportVisitor implements ReportVisitor {
+    @Override
+    public String visitProductReport(Product product) {
+        return "<" + product.getName() + "><" + product.getBarcode() + "><" + product.getCount() + ">";
+    }
+
+    @Override
+    public String visitOrderReport(Order order) {
+        return "<" + order.getDate() + "><" + order.getAddress() + "><" + order.getSumm() + ">";
+    }
+
+    @Override
+    public String visitEmployeeReport(Employee employee) {
+        return "<" + employee.getFullName() + "><" + employee.getPosition() + "><" + employee.getSalary() + ">";
+    }
+}
